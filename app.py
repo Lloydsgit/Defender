2ab743b40263faad86f527d391140bf523ad9e97
Lloydsgit <tejasreddynukella@gmail.com>
2025-07-22 17:07:37 +0530

    protocol section updated

--- app.py
+++ app.py
@@ -62,6 +62,17 @@ DUMMY_CARDS = {
 }
 
 PROTOCOLS = {
+    "POS Terminal -101.1 (4-digit approval)": 4,
+    "POS Terminal -101.4 (6-digit approval)": 6,
+    "POS Terminal -101.6 (Pre-authorization)": 6,
+    "POS Terminal -101.7 (4-digit approval)": 4,
+    "POS Terminal -101.8 (PIN-LESS transaction)": 4,
+    "POS Terminal -201.1 (6-digit approval)": 6,
+    "POS Terminal -201.3 (6-digit approval)": 6,
+    "POS Terminal -201.5 (6-digit approval)": 6
+}
+
+PROTOCOLS = {
      "101.1": 4,
     "101.2": 6,
     "101.3": 6,
